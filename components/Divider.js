bd730f28f14e98c4b5185a0d8efc94b13fc44af0
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-11-14 16:09:09 +0100

    bug fix

--- components/Divider.js
+++ components/Divider.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from '../app/page.module.css'
 const Divider = ({text}) => {
   return (
-    <div style={{height:'200px',width:'100%',background:'#32de84',color:'black',margin:'20px 0',display:'flex',justifyContent:'center',alignItems:'center'}}>
-        <h3 style={{fontSize:'1.7rem',lineHeight:'40pxs'}} className={styles.titeln}>{text}</h3>
+    <div style={{height:'200px',width:'100%',background:'#32de84',color:'black',margin:'20px 0',display:'flex',justifyContent:'center',alignItems:'center',overflow:'hidden'}}>
+        <h3 style={{fontSize:'1.7rem',lineHeight:'40px',maxWidth:'95%'}} className={styles.titeln}>{text}</h3>
     </div>
   )
 }

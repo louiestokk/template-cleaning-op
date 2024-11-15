a914f2dfcbd5231a46f674e9c76f2f25baefd496
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-11-15 22:21:23 +0100

    updates

--- components/TjansterCarousal.js
+++ components/TjansterCarousal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid2,Box,Button} from '@mui/material'
 import styles from '../app/page.module.css'
+import {services} from '../content/tjanster'
 const TjansterCarousal = () => {
   return (
     <div style={{background:'#32de84',height:'620px',width:'100%'}}>
@@ -25,35 +26,3 @@ const TjansterCarousal = () => {
 
 export default TjansterCarousal
 
-const services = [
-    {
-        service:'Städning',
-        path:'#stadning',
-        image:'https://img.freepik.com/free-vector/cleaning-team-with-brooms-vacuum-cleaner-man-woman-uniform-with-professional-equipment-ready-work-together-flat-vector-illustration-cleaning-service-teamwork-occupation-concept_74855-24396.jpg?t=st=1731534368~exp=1731537968~hmac=e9e1fa2e70f3c196ef98bf5a4f5c62d446a915625b9d46b53643da290b52d42d&w=1800',
-        text:'Låt oss ta hand om din städning! Vi erbjuder allt från regelbunden hemstädning till storstädning och flyttstädning – alltid med noggrannhet och omtanke. Med RUT-avdraget får du professionell hjälp till ett förmånligt pris, så att du kan njuta av ett skinande rent hem utan stress.'
-    },
-    {
-        service:'Flytt',
-        path:'#flytt',
-        image:'https://img.freepik.com/free-vector/flat-design-house-moving-concept-with-truck_23-2148665508.jpg?t=st=1731534391~exp=1731537991~hmac=8c81718ac784117533910e43b2f0322fc33935244d2aba4b42d87fcdde169d39&w=1800',
-        text:'Vi hjälper dig genom hela flytten! Från packning och transport till uppackning – Tant Rut ser till att din flytt blir smidig och säker. Med oss kan du slappna av och känna dig trygg, och med RUT-avdraget blir det dessutom mer prisvärt.'
-    },
-    {
-        service:'Bortforsling',
-        path:'#bortforsling',
-        image:'https://img.freepik.com/premium-vector/moving-house-woman-packing-stuff-move-new-house-apartment-men-carrying-sofa-cardboard-box-illustration_333239-156.jpg?w=1380',
-        text:'Behöver du hjälp med att tömma och forsla bort gammalt bohag eller annat som inte längre behövs? Tant Rut erbjuder pålitlig bortforsling av möbler, skräp och dödsbon, med omtanke och respekt för både miljö och hem. Med RUT-avdraget blir det enklare och billigare för dig att få en nystart.'
-    },
-    {
-        service:'Dödsbon',
-        path:'#dodsbo',
-        image:'https://img.freepik.com/free-vector/repair-construction-works-father-son-man-boy-holding-fix-tools-standing-near-family-house-flat-vector-illustration-renovation-concept-banner-website-design-landing-web-page_74855-23155.jpg?t=st=1731534605~exp=1731538205~hmac=7ec967a871315c35c329ab11433da30fe1e87911705dae1b75df18b3fb815a0f&w=1380',
-        text:'Vi på Tant Rut erbjuder ett komplett stöd vid hantering av dödsbon – vi hjälper dig att tömma, städa, värdera och köpa upp hela eller delar av bohaget. Vårt team bemöter varje uppdrag med stor respekt och känslighet för situationen, och med RUT-avdraget kan vi erbjuda en kostnadseffektiv lösning.'
-    },
-    {
-        service:'Kontorsstäd',
-        path:'#kontorsstad',
-        image:'https://img.freepik.com/free-vector/woman-working-office-industrial-alpinist-cleaning-window_778687-1239.jpg?t=st=1731534663~exp=1731538263~hmac=3480bf6628b2eb3805120287834c5870809873d0a2c9b4f1c5b792552c8cd3a6&w=1800',
-        text:'Ett rent och välstädat kontor gör stor skillnad för både trivsel och produktivitet. Tant Rut erbjuder professionell kontorsstädning anpassad efter era behov och arbetstider. Vi ser till att skapa en fräsch och inbjudande arbetsmiljö, så att ni kan fokusera på ert arbete.'
-    }
-]
---
+++ content/tjanster.js
@@ -0,0 +1,68 @@
+export const services = [
+    {
+        service:'Städning',
+        path:'#stadning',
+        image:'https://img.freepik.com/free-vector/cleaning-team-with-brooms-vacuum-cleaner-man-woman-uniform-with-professional-equipment-ready-work-together-flat-vector-illustration-cleaning-service-teamwork-occupation-concept_74855-24396.jpg?t=st=1731534368~exp=1731537968~hmac=e9e1fa2e70f3c196ef98bf5a4f5c62d446a915625b9d46b53643da290b52d42d&w=1800',
+        text:'Låt oss ta hand om din städning! Vi erbjuder allt från regelbunden hemstädning till storstädning och flyttstädning – alltid med noggrannhet och omtanke. Med RUT-avdraget får du professionell hjälp till ett förmånligt pris, så att du kan njuta av ett skinande rent hem utan stress.'
+    },
+    {
+        service:'Flytt',
+        path:'#flytt',
+        image:'https://img.freepik.com/free-vector/flat-design-house-moving-concept-with-truck_23-2148665508.jpg?t=st=1731534391~exp=1731537991~hmac=8c81718ac784117533910e43b2f0322fc33935244d2aba4b42d87fcdde169d39&w=1800',
+        text:'Vi hjälper dig genom hela flytten! Från packning och transport till uppackning – Tant Rut ser till att din flytt blir smidig och säker. Med oss kan du slappna av och känna dig trygg, och med RUT-avdraget blir det dessutom mer prisvärt.'
+    },
+    {
+        service:'Bortforsling',
+        path:'#bortforsling',
+        image:'https://img.freepik.com/premium-vector/moving-house-woman-packing-stuff-move-new-house-apartment-men-carrying-sofa-cardboard-box-illustration_333239-156.jpg?w=1380',
+        text:'Behöver du hjälp med att tömma och forsla bort gammalt bohag eller annat som inte längre behövs? Tant Rut erbjuder pålitlig bortforsling av möbler, skräp och dödsbon, med omtanke och respekt för både miljö och hem. Med RUT-avdraget blir det enklare och billigare för dig att få en nystart.'
+    },
+    {
+        service:'Dödsbon',
+        path:'#dodsbo',
+        image:'https://img.freepik.com/free-vector/repair-construction-works-father-son-man-boy-holding-fix-tools-standing-near-family-house-flat-vector-illustration-renovation-concept-banner-website-design-landing-web-page_74855-23155.jpg?t=st=1731534605~exp=1731538205~hmac=7ec967a871315c35c329ab11433da30fe1e87911705dae1b75df18b3fb815a0f&w=1380',
+        text:'Vi på Tant Rut erbjuder ett komplett stöd vid hantering av dödsbon – vi hjälper dig att tömma, städa, värdera och köpa upp hela eller delar av bohaget. Vårt team bemöter varje uppdrag med stor respekt och känslighet för situationen, och med RUT-avdraget kan vi erbjuda en kostnadseffektiv lösning.'
+    },
+    {
+        service:'Kontorsstäd',
+        path:'#kontorsstad',
+        image:'https://img.freepik.com/free-vector/woman-working-office-industrial-alpinist-cleaning-window_778687-1239.jpg?t=st=1731534663~exp=1731538263~hmac=3480bf6628b2eb3805120287834c5870809873d0a2c9b4f1c5b792552c8cd3a6&w=1800',
+        text:'Ett rent och välstädat kontor gör stor skillnad för både trivsel och produktivitet. Tant Rut erbjuder professionell kontorsstädning anpassad efter era behov och arbetstider. Vi ser till att skapa en fräsch och inbjudande arbetsmiljö, så att ni kan fokusera på ert arbete.'
+    },
+    {
+        service:'Hemstäd',
+        path:'/stadfirma-goteborg',
+        image:'https://img.freepik.com/free-vector/housewife-cleaning-home-with-vacuum-cleaner-sofa-house-room-flat-vector-illustration-household-housekeeping_74855-13139.jpg?t=st=1731705191~exp=1731708791~hmac=cda90ffcdd2247f67213e6798e4d6b69d4b8961dc010a378193c0e25fbc5af6c&w=2000',
+        text:'Ett rent och välstädat hem gör stor skillnad för trivsel, energi och vardagskvalitet. På Tant Rut erbjuder vi professionell hemstädning som anpassas efter dina behov och önskemål. Våra erfarna städare ser till att skapa en fräsch, inbjudande hemmiljö där du kan koppla av och fokusera på det som är viktigt för dig.'
+    },
+    {
+        service:'Flyttstäd',
+        path:'/flyttstad-goteborg',
+        image:'https://img.freepik.com/free-vector/women-cleaning-apartment-professional-female-cleaners-overalls-sanitary-service-housekeeping-business-vacuum-cleaning-dust-wiping_575670-1481.jpg?t=st=1731705547~exp=1731709147~hmac=fba0e8913fdbd8d90b5185bc803bbda0b3f20d8fe8d5c117e78c4dcd2d74f52e&w=2000',
+        text:'En noggrant utförd flyttstädning är avgörande för att lämna en bostad i bästa skick, vilket underlättar överlämningen och ökar chansen att få tillbaka depositionen. Tant Rut erbjuder professionell flyttstädning som uppfyller alla krav och riktlinjer för en smidig och godkänd städning. '
+    },
+    {
+        service:'Tömma dödsbon',
+        path:'/tomma-dodsbo-goteborg',
+        image:'https://img.freepik.com/free-vector/happy-people-moving-into-new-home-cartoon-characters-carrying-carton-boxes-sofa-indoors_74855-10777.jpg?t=st=1731705292~exp=1731708892~hmac=578decbebea6a1588a0b2a2dd4f3f4af90bface4ce7a0e1750502bcd4c25048b&w=2000',
+        text:'Att tömma ett dödsbo är ofta en känslig och arbetskrävande process. Tant Rut erbjuder professionell hjälp med att tömma dödsbo, och vi hanterar varje uppdrag med respekt, noggrannhet och omsorg. Våra erfarna medarbetare hjälper dig att sortera, packa och transportera tillhörigheter enligt dina önskemål.'
+    },
+    {
+        service:'Sälja dödsbon',
+        path:'/salja-dodsbo-goteborg',
+        image:'https://img.freepik.com/free-vector/character-illustration-people-selling-house_53876-64663.jpg?t=st=1731705314~exp=1731708914~hmac=acea5de582014ad93632bf1549e10994f20aeb4cd92c94feda7f5ed287bc80b3&w=1800',
+        text:'Att sälja ett dödsbo kan vara en känslomässigt krävande process med många praktiska detaljer att ta hänsyn till. Tant Rut erbjuder professionell hjälp med att sälja dödsbo, och vi hanterar varje steg med respekt och omsorg.'
+    },
+    {
+        service:'Värdera dödsbon',
+        path:'/vardera-dodsbo-goteborg',
+        image:'https://img.freepik.com/premium-vector/accounting-auditing-service-business-man-looking-through-magnifying-glass-bill-check_566886-10598.jpg?w=1380',
+        text:'Att värdera ett dödsbo kan vara både känslomässigt och praktiskt utmanande. Tant Rut erbjuder professionell och respektfull värdering av dödsbo för att underlätta denna process. Våra erfarna medarbetare gör en noggrann genomgång av inventarierna och ger en rättvis värdering.'
+    },
+    {
+        service:'Köper dödsbon',
+        path:'/kopa-dodsbo-goteborg',
+        image:'https://img.freepik.com/free-vector/flat-design-cash-delivery_23-2148806263.jpg?t=st=1731705384~exp=1731708984~hmac=157757ba417922bc61a20b158f0e366cc11efd289737ce6b5c24c86494172d2f&w=1380',
+        text:'Att avveckla ett dödsbo kan vara en känslig och tidskrävande process. På Tant Rut erbjuder vi en smidig lösning genom att köpa hela dödsbon, vilket gör processen enklare för dig. Våra erfarna medarbetare genomför en noggrann och rättvis värdering av alla tillhörigheter.'
+    }
+]

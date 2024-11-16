7fe1df81e438a1c67cab527456f5549cdb09e2cf
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-11-16 14:32:52 +0100

    fix

--- components/PopularaBtns.js
+++ components/PopularaBtns.js
@@ -5,7 +5,7 @@ import styles from '../app/page.module.css'
 const PopularaBtns = ({servicesArrayPop}) => {
   return (
     <div style={{width:'100%',height:'100%',position:'relative',marginTop:'100px',marginBottom:'100px'}}>
-      <svg className={styles.svg} style={{position:'absolute',top:'-11%'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill='#32de84' fillOpacity="1" d="M0,224L48,234.7C96,245,192,267,288,256C384,245,480,203,576,186.7C672,171,768,181,864,154.7C960,128,1056,64,1152,85.3C1248,107,1344,213,1392,266.7L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      <svg className={styles.svg} style={{position:'absolute',top:'-11.5%'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill='#32de84' fillOpacity="1" d="M0,224L48,234.7C96,245,192,267,288,256C384,245,480,203,576,186.7C672,171,768,181,864,154.7C960,128,1056,64,1152,85.3C1248,107,1344,213,1392,266.7L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
      <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',background:'#32de84',height:'650px'}}>
          <h2 style={{fontFamily:'Georgia,sans-serif',maxWidth:'80%',marginBottom:'2rem'}}>Populära sökningar</h2>
          {servicesArrayPop &&    <div style={{width:'100%',display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',flexWrap:'wrap'}}>
